9bb19e5a14a2fc5cdee98c8d6eb5b0150483cb0f
Logan Willaumez <Logan.willaumez@gmail.com>
2021-11-06 13:47:50 +0100

    add rules for email input

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector('.free-trial');
 const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const inputSelect = document.querySelectorAll('input');
-const inputContain = document.querySelectorAll('.input-container')
+const emailError = document.querySelector('.email-error');
+const inputContain = document.querySelectorAll('.input-container');
 const errorList = document.querySelectorAll('.error');
 let value = [];
 btn.addEventListener('click',(e)=>{
@@ -11,19 +12,30 @@ btn.addEventListener('click',(e)=>{
     })
     verify(value);
     value = [];
-    console.log(value)
-
 })
 
 let verify = (e)=>{
     for (let i=0;i<4;i++){
-        if(e[i]== ''){
-            inputContain[i].classList.remove('active')
-            void inputContain[i].offsetWidth
-            inputContain[i].classList.add('active')
+        if(e[i]=== ''){
+           resetAnim(i);
+           if(e[2]=== ''){
+                     emailError.innerText = "E-mail cannot be embpty";
+           }
         }
         else{
-            inputContain[i].classList.remove('active')
+           inputContain[i].classList.remove('active')
+        if (!e[2].match(regExp)){
+                emailError.innerText = "Look like this is not an email";
+                resetAnim(2);
+            }
+            else{
+                inputContain[2].classList.remove('active')
+            }
         }
     }
+}
+let resetAnim = (e) =>{
+    inputContain[e].classList.remove('active')
+    void inputContain[e].offsetWidth
+    inputContain[e].classList.add('active')
 }
